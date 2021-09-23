5aadcb67c7ce994228bef33a7b6312c0632429d7
AHMAD <57689837+ahmadner@users.noreply.github.com>
2021-09-23 13:01:12 +0300

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -1,22 +1,18 @@
 // 14. Write a c++ Program to accept a string in any case and print it by another case.
 
 #include <iostream>
-
 using namespace std;
-
 int main(){
 
 	char ch;
-	cout<<"enter a string :";
-	while(( ch=getchar( ))!='\n'){
-	if(ch>='A' && ch<='Z')
-	putchar(ch+32);
-	else
-	if(ch>='a' && ch<='z')
-	putchar(ch-32);
-	else
-	putchar(ch);
+	cout<<"Enter the Text: ";
+	while ((ch=getchar())!='\n'){
+		if (ch==' ')
+			putchar(ch);
+		if (ch>='A' && ch<='Z')
+			putchar(ch+32);
+		else if (ch>='a' && ch<='z')
+			putchar(ch-32);
 	}
-	cout<<" is the string";
-	return 0;
+	cout<<"\n";
 }
