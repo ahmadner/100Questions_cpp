edd6d1e875d13ae2617ff3fed50f236009515986
AHMAD <57689837+ahmadner@users.noreply.github.com>
2021-09-23 13:30:58 +0300

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -1,5 +1,17 @@
+// 15. Write C++ Program to check whether a given number is perfect or not.
 #include <iostream>
 using namespace std;
-int int main() {
-  
+int main() {
+	long inputNum,checkNum=0;
+	cout <<"[*] check whether a given number is perfect or not\n\n";
+	cout<<"Enter the number: ";cin>>inputNum;
+
+	for (long i=1;i<=(inputNum/2);i++)
+		if (inputNum%i==0)
+			checkNum+=i;
+
+	if (checkNum == inputNum)
+		cout<<"\n[*] number "<<inputNum<<" is perfect\n\n";
+	else
+		cout<<"\n[*] number "<<inputNum<<" is NOT perfect\n\n";
 }
