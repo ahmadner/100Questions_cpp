cd12cd62a302cb7dcf4b859302c26fd164dd3a9e
AHMAD <57689837+ahmadner@users.noreply.github.com>
2021-09-23 17:45:05 +0300

    Create 19.cpp

---
+++ 19.cpp
@@ -0,0 +1,25 @@
+// 19. Write a c++ Program to accept a string in upper case and print it by lower case.
+
+// input : HelLO UsEr
+// output: hello user
+
+#include <iostream>
+using namespace std;
+int main(){
+
+	char ch,CH;
+	cout <<"Enter text in upper case: ";
+
+	while ((CH=getchar())!='\n'){
+		if ((CH>='A' && CH<='Z') || CH==' '){
+			if (CH == ' ')
+				putchar(CH);
+			else{
+				ch=CH+32;
+				putchar(ch);
+			}
+		}
+		else{ 
+			putchar(CH);}
+	}
+}
